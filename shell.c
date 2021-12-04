6c55330397935caa42113e168a8dce94a81b3cad
elkinguerrero007 <3439@holbertonschool.com>
2021-12-04 03:50:35 +0000

    toquenizando

--- shell.c
+++ shell.c
@@ -4,8 +4,12 @@ void shell(int ac, char **av, char **env)
 
   char *tmp = NULL;
   char *line;
+  char **args;
   char *er;
   char *filename;
+
+  er = "Error";
   do {
 		prompt();
 		line = _getline();
+                args = split_line(line);
