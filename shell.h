ae28fc719cd5bda2bf01f4deec00cbc30805c0b0
elkinguerrero007 <3439@holbertonschool.com>
2021-12-09 03:25:17 +0000

    prototipos

--- shell.h
+++ shell.h
@@ -5,13 +5,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <dirent.h>
 #include <stddef.h>
+#include <errno.h>
 #include <sys/type.h>
+#include <sys/wait.h>
 
 #define TOKENS_BUFFER_SIZE 64
 #define LINE_SIZE 1024
 #define TOKEN_DELIMITERS " \t\r\n\a"
-
+extern char **environ;
 /**
  * struct builtins - Has builtins and associated funcs
  * @arg: Builtins name
@@ -27,12 +30,19 @@ void shell(int ac, char **av, char **env);
 void prompt(void);
 char *_getline(void);
 char **split_line(char *line);
+int execute_prog(char **args, char *line, char **env, int flow);
 int bridge(char *check, char **args);
 int builtins_checker(char **args);
+char *save_path(char *tmp, char *path);
+char *read_dir(char *er, struct dirent *s, char *fi, int l, char *p, char *t);
 void exit_shell(char **args, char *line, char **env);
 void env_shell(char **args, char *line, char **env);
+int _strcmp(char *s1, char *s2);
+char *find_path(char *args, char *tmp, char *er);
+char *search_cwd(char *filename, char *er);
 char *_getenv(char *env);
+char *_strstr(char *haystack, char *needle);
 int launch_prog(char **args);
 int check_for_builtins(char **args, char *line, char **env);
-
+int _strlen(char *s);
 #endif
