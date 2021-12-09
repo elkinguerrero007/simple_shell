ac90283fa51627354c15974628dc9e5f7aa46599
cristian <cristaker1927@hotmail.com>
2021-12-08 20:11:28 -0800

    shell

--- shell.c
+++ shell.c
@@ -1,4 +1,11 @@
 #include "shell.h"
+/**
+ * shell - Infinite loop that runs shell
+ * @ac: Arg count
+ * @av: args passed to shell at beginning of prog
+ * @env: Environment
+ * Return: Void
+ * **/
 
 void shell(int ac, char **av, char **env)
 
@@ -17,3 +24,13 @@ void shell(int ac, char **av, char **env)
                 args = split_line(line);
                 flow = bridge(args[0], args);
 		if (flow == 2)
+		{
+			filename = args[0];
+			args[0] = find_path(args[0], tmp, er);
+			if (args[0] == er)
+			{
+				args[0] = search_cwd(filename, er);
+				if (args[0] == filename)
+					write(1, er, 5);
+			}
+		}
