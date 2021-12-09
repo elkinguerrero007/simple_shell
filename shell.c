4103d9e780826360546acee483d1ea12c17041a9
elkinguerrero007 <3439@holbertonschool.com>
2021-12-09 05:07:20 +0000

    betty

--- shell.c
+++ shell.c
@@ -9,42 +9,42 @@
 
 void shell(int ac, char **av, char **env)
 {
-  char *tmp = NULL;
-  char *line;
-  char **args;
-  char *er;
-  char *filename;
-  int flow;
-  int status = 1;
+char *tmp = NULL;
+char *line;
+char **args;
+char *er;
+char *filename;
+int flow;
+int status = 1;
 
-  er = "Error";
-  do {
-		prompt();
-		line = _getline();
-                args = split_line(line);
-                flow = bridge(args[0], args);
-		if (flow == 2)
+er = "Error";
+do {
 
-		{
-			filename = args[0];
-			args[0] = find_path(args[0], tmp, er);
-			if (args[0] == er)
-			{
-				args[0] = search_cwd(filename, er);
-				if (args[0] == filename)
-					write(1, er, 5);
-			}
-		}
-		if (args[0] != er)
-		status = execute_prog(args, line, env, flow);
-		free(line);
-		free(args);
-	} while (status);
-	if (!ac)
-		(void)ac;
-	if (!av)
-		(void)av;
-	if (!env)
-		(void)env;
-}  
+prompt();
+line = _getline();
+args = split_line(line);
+flow = bridge(args[0], args);
+if (flow == 2)
 
+{
+filename = args[0];
+args[0] = find_path(args[0], tmp, er);
+if (args[0] == er)
+{
+args[0] = search_cwd(filename, er);
+if (args[0] == filename)
+write(1, er, 5);
+}
+}
+if (args[0] != er)
+status = execute_prog(args, line, env, flow);
+free(line);
+free(args);
+} while (status);
+if (!ac)
+(void)ac;
+if (!av)
+(void)av;
+if (!env)
+(void)env;
+}
